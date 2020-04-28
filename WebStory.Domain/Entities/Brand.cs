2ada329f5f36c938fc7ae7d6d0ce654a253b9ca6
Timur Nasibullin <timur_nasibullin@mail.ru>
2020-04-28 09:24:17 +0300

    Add brand and section classes to domain project

---
+++ WebStory.Domain/Entities/Brand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStory.Domain.Entities.Base;
+using WebStory.Domain.Entities.Base.Interfaces;
+
+namespace WebStory.Domain.Entities
+{
+    /// <summary>Бренд</summary>
+    class Brand : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+    }
+}
