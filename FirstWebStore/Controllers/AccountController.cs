6db8bcc35b267c31c15fafb9f9bcce1cc205058d
Timur Nasibullin <timur_nasibullin@mail.ru>
2020-05-05 14:26:37 +0300

    Add logic of user registration

--- FirstWebStore/Controllers/AccountController.cs
+++ FirstWebStore/Controllers/AccountController.cs
@@ -1,13 +1,55 @@
-﻿using FirstWebStore.ViewModels.Identity;
+﻿using System.Threading.Tasks;
+using FirstWebStore.ViewModels.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebStory.Domain.Entities.Identity;
 
 namespace FirstWebStore.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _UserManager;
+        private readonly SignInManager<User> _SignInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _UserManager = userManager;
+            _SignInManager = signInManager;
+        }
+
         public IActionResult Register()
         {
             return View(new RegisterUserViewModel());
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new User
+            {
+                UserName = model.UserName
+            };
+
+            var registerResult = await _UserManager.CreateAsync(user, model.Password);
+
+            if (registerResult.Succeeded)
+            {
+                await _SignInManager.SignInAsync(user, false);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in registerResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View(model);
         }
 
         public IActionResult Login()
