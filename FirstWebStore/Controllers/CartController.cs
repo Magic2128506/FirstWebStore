c5b4cdf043f10eb9980b4c5886fc07df4845dea4
Timur Nasibullin <timur_nasibullin@mail.ru>
2020-05-12 16:48:08 +0300

    Add view and controller for cart service

---
+++ FirstWebStore/Controllers/CartController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstWebStore.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public IActionResult Details() => View(_cartService.TransformFromCart());
+
+        public IActionResult AddToCart(int id)
+        {
+            _cartService.AddToCart(id);
+
+            return RedirectToAction(nameof(Details));
+        }
+
+        public IActionResult DeсrementFromCart(int id)
+        {
+            _cartService.DecrementFromCart(id);
+
+            return RedirectToAction(nameof(Details));
+        }
+
+        public IActionResult RemoveFromCart(int id)
+        {
+            _cartService.RemoveFromCart(id);
+
+            return RedirectToAction(nameof(Details));
+        }
+
+        public IActionResult RemoveAll()
+        {
+            _cartService.RemoveAll();
+
+            return RedirectToAction(nameof(Details));
+        }
+    }
+}
