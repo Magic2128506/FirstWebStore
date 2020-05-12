fcac188b9599ae61149727db221c9ae479483c42
Timur Nasibullin <timur_nasibullin@mail.ru>
2020-05-12 11:34:08 +0300

    Add cart service description

---
+++ FirstWebStore/ViewModels/CartViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FirstWebStore.ViewModels
+{
+    public class CartViewModel
+    {
+        public Dictionary<ProductViewModel, int> Items { get; set; } = new Dictionary<ProductViewModel, int>();
+
+        public int ItemsCount => Items?.Sum(i => i.Value) ?? 0;
+    }
+}
